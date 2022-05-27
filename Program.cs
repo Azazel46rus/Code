ae3bccea4789a39bca3b730d3e772af9b0f582ba
Maxim <103738785+Azazel46rus@users.noreply.github.com>
2022-05-27 12:00:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,32 +10,35 @@ namespace Homework_Theme_03
             Console.WriteLine("Введите имя первого игрока");
             string player1 = Console.ReadLine();
             Console.WriteLine();
+
             Console.WriteLine("Введите имя второго игрока");
             string player2 = Console.ReadLine();
             Console.WriteLine();
 
             Random random = new Random();
 
-            int randomNumber = random.Next(12, 120);
+            int curentValue = random.Next(12, 120);
 
-            Console.WriteLine($"Число {randomNumber}, ваша цель добраться до 0 используя 1, 2, 3 или 4");
+            Console.WriteLine($"Число {curentValue}, ваша цель добраться до 0 используя 1, 2, 3 или 4");
 
             int player1Number = 0;
             int player2Number = 0;
 
-            while (randomNumber != 0)
+            while ( curentValue > 0 && curentValue != 0)
             {
                 Console.WriteLine($"Ход игрока {player1}");
                 player1Number = int.Parse(Console.ReadLine());
-                if (player1Number < 0 || player1Number > 4)
+
+                while (player1Number < 0 || player1Number > 4)
                 {
                     Console.WriteLine("Вы ввели недопустимое значение. Повторите попытку");
-                    continue;
+                    player1Number = int.Parse(Console.ReadLine());
                 }
-                randomNumber = randomNumber - player1Number;
+
+                curentValue = curentValue - player1Number;
                 Console.WriteLine();
-                Console.WriteLine($"Остаток = {randomNumber}");
-                if (randomNumber == 0)
+                Console.WriteLine($"Остаток = {curentValue}");
+                if (curentValue == 0 || curentValue < 0)
                 {
                     Console.WriteLine($"Поздравляем {player1} с победой. {player2} может реванш?");
                     break;
@@ -43,15 +46,16 @@ namespace Homework_Theme_03
 
                 Console.WriteLine($"Ход игрока {player2}");
                 player2Number = int.Parse(Console.ReadLine());
-                if (player2Number < 0 || player2Number > 4)
+                while (player2Number < 0 || player2Number > 4)
                 {
                     Console.WriteLine("Вы ввели недопустимое значение. Повторите попытку");
-                    continue;
+                    player2Number = int.Parse(Console.ReadLine());
                 }
-                randomNumber = randomNumber - player2Number;
+                curentValue = curentValue - player2Number;
                 Console.WriteLine();
-                Console.WriteLine($"Остаток = {randomNumber}");
-                if (randomNumber == 0)
+                Console.WriteLine($"Остаток = {curentValue}");
+
+                if (curentValue == 0 || curentValue < 0)
                 {
                     Console.WriteLine($"Поздравляем {player2} с победой. {player1} может реванш?");
                     break;
